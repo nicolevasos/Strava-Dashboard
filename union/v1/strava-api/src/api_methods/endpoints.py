d433efa1ff8476e92a120c13f452f5a78bceb9ec
Nicole Salazar Cuellar <134808140+nicolevasos@users.noreply.github.com>
2025-08-22 11:42:43 +0200

    Delete union/v1 directory

--- union/v1/strava-api/src/api_methods/endpoints.py
+++
@@ -1,2 +0,0 @@
-auth_endpoint:str = "https://www.strava.com/oauth/token"
-activites_endpoint:str = "https://www.strava.com/api/v3/athlete/activities"
--- union/v1/strava-api/src/data_preprocessing/main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-def preprocess_data(data:dict) -> pd.DataFrame:
-
-    return pd.json_normalize(data)
-
